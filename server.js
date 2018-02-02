64abd9e4080cc318968643930e74e50c9eaf1c3c
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-02-01 20:59:41 -0500

    basic template set up

---
+++ server.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const nunjucks = require('nunjucks');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+nunjucks.configure({ noCache: true });
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+app.use(bodyparser.urlencoded());
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
+
+app.get('/', (req, res, next) => {
+  res.render('index')
+})
